03dce23ae9769eae0c9c1ab8b040b40f9c8fe718
AnzoBenjamin <anzobnjmn@gmail.com>
2023-09-01 03:21:37 +0300

    Fixing vulnerabilities

--- src/components/Main/Events/EventsHero.tsx
+++ src/components/Main/Events/EventsHero.tsx
@@ -1,11 +1,21 @@
 import React from "react";
-import classes from "./EventsHero.module.scss"
+import classes from "./EventsHero.module.scss";
 const EventsHero: React.FC = () => {
-  return <header className={classes.hero}>
-    <div className={classes["hero-text"]}>
-    <h1 className={classes["heading-primary"]}>Unleash Your Potential</h1>
-    </div>
-  </header>;
+  return (
+    <header className={classes.hero}>
+      <div className={classes["hero-text"]}>
+        <div>
+          <h1 className={classes["heading-primary"]}>Unleash Your Potential</h1>
+          <h3 className={classes["heading-secondary"]}>
+            Welcome to a world where creativity knows no bounds and passions
+            take center stage. Our platform is dedicated to providing a vibrant
+            space for individuals to showcase their remarkable talents and
+            skills.{" "}
+          </h3>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default EventsHero;
